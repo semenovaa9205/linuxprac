04006c8fbba430880aba47d7296a9e1e030f9d86
semenovaa9205 <andrey.semenov2305@mail.ru>
2021-11-06 18:02:26 +0300

    test02fail

--- lec02dz/test/test02.c
+++ lec02dz/test/test02.c
@@ -1,9 +1,10 @@
- #include<stdio.h>
- #include<ncurses.h>
+ #include<locale.h>
+ #include<curses.h>
  int main(){
+setlocale(LC_ALL , "");
 initscr();
 addstr("привет ,мир");
+getch();
 endwin();
-
-     return 0; 
+return 0; 
  }
