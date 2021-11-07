c73cfd6cc706443c3a1e1908d06dc1e08f25cfca
semenovaa9205 <andrey.semenov2305@mail.ru>
2021-11-07 17:19:31 +0300

    sample window

--- lec02dz/lec02dz.c
+++ lec02dz/lec02dz.c
@@ -1,12 +1,23 @@
 
-
-#include<stdio.h>
-#include <ncurses.h>
-
-
+//#include<locale.h>
+//#include<stdio.h>
+#include <curses.h>
 int main(){
-    
-initscr();
-addstr("hello,world");
+ const int def=3;
+ int sumbol=0;
+ WINDOW*win;
+ win=newwin (LINES-2*def,COLS-2*def,def,def);
+ keypad(win,TRUE);
+ scrollok(win,TRUE);
+ do{  
+ 
+     wprintw(win,"Key: %d,Name: %s/n",sumbol,keyname(sumbol));
+     box(win,0,0);
+     wrefresh(win);
+ }while(sumbol=wgetch((win))!=27);
+
+
+endwin();
 return 0; 
 }
+
