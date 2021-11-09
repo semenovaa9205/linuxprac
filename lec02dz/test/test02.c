40e9c5081094ce6322907cf57f527073b86a2bf3
semenovaa9205 <andrey.semenov2305@mail.ru>
2021-11-09 15:08:33 +0300

    Revert "test02"
    This reverts commit b4340f143febca3097c08d527d6cd943d2cc3d73.

--- lec02dz/test/test02.c
+++ lec02dz/test/test02.c
@@ -1,14 +1,10 @@
  #include<locale.h>
  #include<curses.h>
  int main(){
-     
-
-setlocale(LC_ALL , "ru_RU.UTF-8");
+setlocale(LC_ALL , "");
 initscr();
 addstr("привет ,мир");
 getch();
 endwin();
- 
-
-return 0;
+return 0; 
  }
